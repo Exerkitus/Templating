6385f9077f61e6a6e9370bcb8c33363d465c2439
RabidSheep55 <tharreaupierre@gmail.com>
2020-07-10 13:55:54 +0200

    initial commit

---
+++ generateHTML.py
@@ -0,0 +1,24 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+from os.path import join
+
+# Load sheet data from file
+with open("Sheet.json", 'r') as file:
+    sheet = json.load(file)
+
+# Configure jinja environment
+env = Environment(
+    loader=FileSystemLoader("./templates")
+)
+
+# Load master template from environment
+master = env.get_template("master.html")
+
+# Test render
+renderedHTML = master.render(sheet)
+
+print(renderedHTML)
+
+# Save rendered HTML to File
+with open(join("renders", "index.html"), 'w') as file:
+    file.write(renderedHTML)
